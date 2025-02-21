b7eb8f31267913bfb1029da3c4647e4d6757bdb4
martingerdes <96237951+martingerdes@users.noreply.github.com>
2025-02-21 20:52:35 +0000

    Deploying to gh-pages from @ martingerdes/martingerdes.github.io@6fde33ec7a25cf1ba9867dac8d36e2b1a5b88597 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/editions/";
           },
-        },{id: "post-version-1-1",
+        },{id: "post-vereinigte-staaten-von-europa",
+      
+        title: "Vereinigte Staaten von Europa",
+      
+      description: "Zukunftsvision in Anbetracht des Ausfalls der USA",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/united-states-of-europe/";
+        
+      },
+    },{id: "post-version-1-1",
       
         title: "Version 1.1",
       
